874ff9769147a76beb607a39641ee0e15a42ffcf
Jung-Hyun Kwon <arunson@kaist.ac.kr>
2016-01-07 16:47:16 +0900

    done with ch3

--- cpp_programming/ch3.cpp
+++ cpp_programming/ch3.cpp
@@ -11,16 +11,51 @@
 #include <string>
 using namespace std;
 
-int main(){
+void example(){
     int thisisanum;
     cout <<"enter a num:";
     cin >> thisisanum;
     cout << "You entered: " << thisisanum << "\n";
     
+
     string str;
     cout <<"enter a string:";
     cin >> str;
     cout << "You entered: " << str << "\n";
+    cin.get();
     
+    string my_string;
+    cout <<"enter a string: ";
+    getline(cin,my_string,',');
+    cout << "You entered: " << my_string << endl;
+    cout << cin.get();
     
+}
+
+void printMyName(){
+    string name;
+    cout << "Enter your name:";
+    getline(cin, name, '\n');
+    cout << "Your name is " << name << endl;
+}
+
+void plusTwoNums(){
+    int num1;
+    int num2;
+    cout << "Enter a number:";
+    cin >> num1;
+    cout << "Enter next number:";
+    cin >> num2;
+    cout << "The sum is: " << num1+num2 << endl;
+}
+
+void divided(){
+    float num1, num2;
+    cin >> num1;
+    cin >> num2;
+    cout << num1/num2 << endl;
+}
+
+int main(){
+    divided();
 }

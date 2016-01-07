d16125f5d5df9b90ec867f9182a4944106eb36c8
Jung-Hyun Kwon <arunson@kaist.ac.kr>
2016-01-07 13:56:47 +0900

    Initial Commit

---
+++ cpp_programming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_programming
+//
+//  Created by Junghyun Kwon on 1/7/16.
+//  Copyright © 2016 Junghyun Kwon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
